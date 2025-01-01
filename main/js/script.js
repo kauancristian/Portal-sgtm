5ecb138e1ffd778633a3b2cb2cda120505b57c86
Kauan Cristian <kauancristian.lz08@gmail.com>
2025-01-01 11:27:40 -0300

    Posição de seta ajustada em disp.360px

---
+++ main/js/script.js
@@ -0,0 +1,15 @@
+function abrirMenu(){
+    if(itens.style.display == 'none'){
+        itens.style.display = 'block'
+    }else{
+        itens.style.display = 'none'
+    }
+}
+
+function fecharMenu () {
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    }else{
+        itens.style.display = 'block'
+    }
+}
